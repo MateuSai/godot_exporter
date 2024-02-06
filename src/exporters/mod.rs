13218399e1d03b083a194779d17b5ea46669d4c9
Mateu <mateusai@tuta.io>
2024-02-06 17:02:51 +0100

    windows zip compression

--- src/exporters/mod.rs
+++ src/exporters/mod.rs
@@ -4,23 +4,74 @@ use ini::Ini;
 
 pub mod windows;
 
-pub fn export(project_path: String, godot_path: String, output: String) {
-    println!("Project version: {}", get_version(&project_path));
-
-    let windows_conf = windows::Conf {
-        output_folder: output,
-        project_path: project_path.to_owned(),
-        godot_path: godot_path,
-        project_name: get_project_name(&project_path),
-    };
-    let windows_result = windows::export(windows_conf);
-    if windows_result.is_err() {
-        eprintln!("Error exporting to windows")
-    }
+pub enum Error {
+    IniError(ini::Error),
 }
 
-fn get_project_name(project_path: &str) -> String {
-    get_project_configuration(project_path)
+pub struct ExportPreset {
+    name: String,
+    platform: String,
+}
+
+pub fn export(project_path: String, godot_path: String, output: String) -> Result<(), Error> {
+    let project_configuration = match get_project_configuration(&project_path) {
+        Ok(ini) => ini,
+        Err(e) => return Err(Error::IniError(e)),
+    };
+
+    println!("Project version: {}", get_version(&project_configuration));
+
+    let export_presets = match get_export_presets(&project_path) {
+        Ok(vec) => vec,
+        Err(e) => return Err(Error::IniError(e)),
+    };
+    for export_preset in export_presets {
+        match export_preset.platform.as_str() {
+            "Windows Desktop" => {
+                let windows_conf = windows::Conf {
+                    output_folder: output.to_owned(),
+                    project_path: project_path.to_owned(),
+                    godot_path: godot_path.to_owned(),
+                    project_name: get_project_name(&project_configuration),
+                };
+                let windows_result = windows::export(windows_conf, export_preset);
+                if windows_result.is_err() {
+                    eprintln!("Error exporting to windows");
+                }
+            }
+            _ => {
+                eprintln!("Invalid platform {}", export_preset.platform)
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn get_export_presets(project_path: &str) -> Result<Vec<ExportPreset>, ini::Error> {
+    let mut vec = Vec::new();
+
+    let presets_path = PathBuf::from(project_path).join("export_presets.cfg");
+    let presets_file = Ini::load_from_file(presets_path)?;
+    let mut i: u8 = 0;
+    while let Some(section) = presets_file.section(Some(format!("preset.{}", i))) {
+        println!("Found preset.{}", i);
+        let name = section.get("name").unwrap_or("no name");
+        let platform = section.get("platform").unwrap_or("no platform");
+
+        vec.push(ExportPreset {
+            name: name.to_owned(),
+            platform: platform.to_owned(),
+        });
+
+        i += 1;
+    }
+
+    Ok(vec)
+}
+
+fn get_project_name(project_configuration: &Ini) -> String {
+    project_configuration
         .section(Some("application"))
         .unwrap()
         .get("config/name")
@@ -29,8 +80,8 @@ fn get_project_name(project_path: &str) -> String {
         .replace(" ", "_")
 }
 
-pub fn get_version(project_path: &str) -> String {
-    get_project_configuration(project_path)
+fn get_version(project_configuration: &Ini) -> String {
+    project_configuration
         .section(Some("global"))
         .unwrap()
         .get("version")
@@ -38,13 +89,12 @@ pub fn get_version(project_path: &str) -> String {
         .to_string()
 }
 
-fn get_project_configuration(project_path: &str) -> Ini {
+fn get_project_configuration(project_path: &str) -> Result<Ini, ini::Error> {
     let mut godot_project_file_path = PathBuf::from(project_path);
     godot_project_file_path.push("project.godot");
-    if !godot_project_file_path.is_file() {
-        eprintln!("Can't find project.godot file");
-        std::process::exit(2);
-    }
+    /*  if !godot_project_file_path.is_file() {
+        std::io::Error::
+    } */
 
-    Ini::load_from_file(godot_project_file_path).unwrap()
+    Ini::load_from_file(godot_project_file_path)
 }
--- src/exporters/windows.rs
+++ src/exporters/windows.rs
@@ -1,11 +1,13 @@
 use std::{
+    fs::File,
+    io::{Read, Write},
     path::{Path, PathBuf},
     process::Command,
 };
 
-pub enum Error {
-    GodotCommandError,
-}
+use zip::{write::FileOptions, CompressionMethod, ZipWriter};
+
+use super::ExportPreset;
 
 pub struct Conf {
     pub project_path: String,
@@ -14,29 +16,48 @@ pub struct Conf {
     pub project_name: String,
 }
 
-pub fn export(conf: Conf) -> Result<(), Error> {
+pub fn export(conf: Conf, export_preset: ExportPreset) -> Result<(), Box<dyn std::error::Error>> {
+    let executable_path = PathBuf::from(&conf.output_folder)
+        .join(Path::new(conf.project_name.as_str()))
+        .with_extension("exe");
     let godot_command = Command::new(conf.godot_path)
         .args([
             "--headless",
             "--path",
             conf.project_path.as_str(),
             "--export-release",
-            "Windows Desktop",
-            PathBuf::from(conf.output_folder)
-                .join(Path::new(conf.project_name.as_str()))
-                .to_str()
-                .unwrap(),
+            export_preset.name.as_str(),
+            executable_path.to_str().unwrap(),
         ])
         .spawn();
 
-    match godot_command {
-        Ok(mut child) => {
-            if child.wait().is_err() {
-                return Err(Error::GodotCommandError);
-            }
-        }
-        Err(_) => return Err(Error::GodotCommandError),
-    };
+    godot_command?.wait()?;
+
+    println!("Compressing files...");
+
+    let files_to_compress = vec![
+        executable_path.to_owned(),
+        executable_path.with_extension("pck"),
+    ];
+
+    let options = FileOptions::default().compression_method(CompressionMethod::DEFLATE);
+    let mut zip = ZipWriter::new(File::create(executable_path.with_extension("zip"))?);
+
+    for file_path in &files_to_compress {
+        let file = File::open(file_path)?;
+        zip.start_file(file_path.file_name().unwrap().to_str().unwrap(), options)?;
+
+        let mut buffer = Vec::new();
+        std::io::copy(&mut file.take(u64::MAX), &mut buffer)?;
+
+        zip.write_all(&buffer)?;
+
+        std::fs::remove_file(file_path)?;
+    }
+
+    zip.finish()?;
+
+    println!("Done compressing files!");
 
     Ok(())
 }
